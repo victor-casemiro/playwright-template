fc47b0819b083b45b5d780f98d81c3aa5f797529
Victor Casemiro <victor.casemiro@dtidigital.com>
2024-07-16 10:32:55 -0400

    fix: Fixing issues with git

--- tests/e2e/DemoTests.spec.js
+++ tests/e2e/DemoTests.spec.js
@@ -6,8 +6,8 @@ test.beforeEach(async ({ page }) => {
     "CF-Access-Client-Id": "0188aeafcb0547ae968bff7333f18486.access",
     "CF-Access-Client-Secret":
       "f35c1c3cb656959552ef5ff4813086105c59662a4d87ffb47f19e10350f50eb5",
-    });
-    await page.goto("https://dev-htc-web.hometesterclub-dev.com/us/en"); 
+  });
+  await page.goto("https://dev-htc-web.hometesterclub-dev.com/us/en");
 });
 
 test("Verificar navbar", async ({ page }) => {
@@ -17,6 +17,54 @@ test("Verificar navbar", async ({ page }) => {
   await expect(page.locator('#header_faq_click')).toHaveText('FAQ');
   await expect(page.locator('#header_support_click')).toHaveText('Support'); // We're using locator because we need to specify the element's id
   await expect(page.locator('#header_join_now_click')).toHaveText('Join now'); // We're using locator because there are 2 elements with the same text on the page
+});
+
+test("Verificar footer", async ({ page }) => {
+  await expect(page.getByLabel('Change Country')).toHaveText('Change Country');
+  await expect(page.getByRole('link', { name: 'For Brands' })).toHaveText('For Brands');
+  await expect(page.getByRole('link', { name: 'Disclosure' })).toHaveText('Disclosure');
+  await expect(page.getByRole('link', { name: 'Privacy Policy' })).toHaveText('Privacy Policy');
+  await expect(page.getByRole('link', { name: 'Terms of Use' })).toHaveText('Terms of Use');
+  await expect(page.getByRole('link', { name: 'DMCA' })).toHaveText('DMCA');
+  await expect(page.locator('#cookie_policy')).toHaveText('Cookie Policy');
+  await expect(page.locator('#faq_footer')).toHaveText('FAQ');
+  await expect(page.locator('#support_footer')).toHaveText('Support');
+  await expect(page.locator('#brand_power_company')).toBeVisible();
+  await expect(page.getByText('© 2024 The Brand Power')).toHaveText('© 2024 The Brand Power Company. All Rights Reserved.');
+});
+
+test("create support with valid email", async ({ page }) => {
+  await page.locator('#header_support_click').click();
+  await page.waitForTimeout(3000);
+
+  await expect(page.url()).toBe("https://dev-htc-web.hometesterclub-dev.com/us/en/support");
+  await page.goto("https://dev-htc-web.hometesterclub-dev.com/us/en/support");
+
+  //check elements on support page
+  await page.getByRole('heading', { name: 'Anything we can help with?' }).isVisible();
+  await page.getByText('Did we answer your question?').isVisible();
+  await page.getByText('Best contact email *').isVisible();
+  await page.getByText('I\'m a brand or agency').isVisible();
+  await page.getByText('Anything we can help with? *').isVisible();
+  await page.getByRole('button', { name: 'Submit' }).isDisabled();
+
+  //fill necesessary field
+  await page.getByPlaceholder('What is your first name?').isVisible().fill('Peter');
+  await page.getByPlaceholder('Type your best email').isVisible.fill('peter@test.com');
+  await page.getByPlaceholder('Questions, comments, high').isVisible.fill('help me to test this');
+
+  //click in I\'m a brand or agency box (Optional)
+  await page.getByRole('checkbox').click();
+  await page.getByText('I\'m a brand or agency').isVisible();
+
+  //accept all coockies
+  await page.getByRole('button', { name: 'Accept All Cookies' }).click();
+  //submit the support
+  await page.getByRole('button', { name: 'Submit' }).click();
+
+  //validate if success modal is visible
+  await page.getByText('Success').isVisible();
+  await page.getByRole('button', { name: 'Got it' }).click();
 });
 
 // ------------------------------------------------------------------
