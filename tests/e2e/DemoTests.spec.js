3dfae583d582d6f49fa87c83527fd99ea59ccd90
GuilhermeSantana2022 <guilherme.santana@dtidigital.com.br>
2024-07-16 10:14:44 -0300

    Update DemoTests.spec.js

--- tests/e2e/DemoTests.spec.js
+++ tests/e2e/DemoTests.spec.js
@@ -1,14 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-  // await page.setViewportSize({ width: 1920, height: 1080 }); // Assuming "desktop" viewport size is 1920x1080
+  //await page.setViewportSize({ width: 1920, height: 1080 }); // Assuming "desktop" viewport size is 1920x1080
   await page.setExtraHTTPHeaders({
     "CF-Access-Client-Id": "0188aeafcb0547ae968bff7333f18486.access",
     "CF-Access-Client-Secret":
       "f35c1c3cb656959552ef5ff4813086105c59662a4d87ffb47f19e10350f50eb5",
-  });
-
-  await page.goto("https://dev-htc-web.hometesterclub-dev.com/us/en/support");
+    });
+    await page.goto("https://dev-htc-web.hometesterclub-dev.com/us/en"); 
 });
 
 test("Verificar navbar", async ({ page }) => {
@@ -21,14 +20,39 @@ test("Verificar navbar", async ({ page }) => {
 });
 
 test("create support with valid email", async ({ page }) => {
-  //await expect(page.locator('#header_support_click')).toHaveText('Support').click(); // We're using locator because we need to specify the element's id
-  await page.getByPlaceholder('What is your first name?').fill('Peter');
-  await page.getByPlaceholder('Type your best email').fill('peter@test.com');
-  await page.getByRole('checkbox').click();
-  await page.locator('div').filter({ hasText: /^Submit$/ }).isEnabled
+  await page.locator('#header_support_click').click(); 
+  await page.waitForTimeout(3000);
+
+  await expect(page.url()).toBe("https://dev-htc-web.hometesterclub-dev.com/us/en/support");
+  await page.goto("https://dev-htc-web.hometesterclub-dev.com/us/en/support"); 
+
+  //check elements on support page
+  await page.getByRole('heading', { name: 'Anything we can help with?' }).isVisible();
+  await page.getByText('Did we answer your question?').isVisible();
+  await page.getByText('Best contact email *').isVisible();
   await page.getByText('I\'m a brand or agency').isVisible();
-  await page.getByPlaceholder('Questions, comments, high').fill('help me to test this').press('Enter');
-  await page.locator('div').filter({ hasText: /^Submit$/ }).isEnabled
+  await page.getByText('Anything we can help with? *').isVisible();
+  await page.getByRole('button', { name: 'Submit' }).isDisabled();
+
+  //fill necesessary field
+  await page.getByPlaceholder('What is your first name?').isVisible().fill('Peter'); 
+  await page.getByPlaceholder('Type your best email').isVisible.fill('peter@test.com');  
+  await page.getByPlaceholder('Questions, comments, high').isVisible.fill('help me to test this');
+
+  //click in I\'m a brand or agency box (Optional)
+  await page.getByRole('checkbox').click(); 
+  await page.getByText('I\'m a brand or agency').isVisible();
+
+ //accept all coockies
+  await page.getByRole('button', { name: 'Accept All Cookies' }).click();
+  //submit the support
+  await page.getByRole('button', { name: 'Submit' }).click();
+
+  //validate if success modal is visible
+  await page.getByText('Success').isVisible();
+  await page.getByRole('button', { name: 'Got it' }).click();
+
+
 
 
   
