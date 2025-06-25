f38dd6403edd311d4ce3357fd14b5db311975f9f
Victor Casemiro <victor.casemiro@dtidigital.com>
2025-06-25 09:06:00 -0400

    first commit

---
+++ tests/e2e/LoginTests.spec.js
@@ -0,0 +1,7 @@
+//TODO: Test Case 1518: Login - Email valido + Senha valida
+
+//TODO: Test Case 1519: Login - Email invalido + Senha valida
+
+//TODO: Test Case 1520: Login - Email valido + Senha invalida
+
+//TODO: Test Case 1521: Login - Em branco
